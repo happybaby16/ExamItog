316f162ea7dc5a49920ea55a653fb9ec0392bc8d
Pavel Nagornev <pavel.nagornev@mail.ru>
2021-07-05 09:20:58 +0300

    Добавьте файлы проекта.

---
+++ Exam/CriticalWay.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam
+{
+    class CriticalWay
+    {
+        public string s = "";
+        List<dynamic[]> ways_weight = new List<dynamic[]>();
+
+        public int out_point { get; set; }
+        public int in_point { get; set; }
+        public int weight { get; set; }
+
+        public void AddData(int out_p, int in_p, int w)
+        {
+            out_point = out_p;
+            in_point = in_p;
+            weight = w;
+        }
+
+        public int FindHardWay(List<Hard> ribs, Hard start_point, int end_point)
+        {
+
+
+
+            int weight = 0;
+            if (start_point.out_point == 1)
+            {
+
+                weight = start_point.weight;
+            }
+
+
+            if (start_point.in_point == end_point)
+            {
+                s = s + start_point.out_point + " - " + start_point.in_point + "|" + start_point.weight + ";";
+                return weight;
+            }
+
+
+
+            foreach (Hard ob in ribs.Where(x => x.out_point == start_point.in_point))
+            {
+                s = s + start_point.out_point + " - " + start_point.in_point + "|" + start_point.weight + "\n";
+                if (start_point.in_point == ob.out_point)
+                {
+
+                    weight = weight + ob.weight;
+                    weight = weight + FindHardWay(ribs, ob, end_point);
+
+
+
+
+                }
+
+            }
+
+            return weight;
+
+        }
+
+
+
+
+        public void Parse()
+        {
+            string[] ways = s.Split(';');
+            for (int i = 0; i < ways.Length - 1; i++)
+            {
+                var a = ways[i].Split('\n');
+                string way = "";
+                int weight = 0;
+                for (int j = 0; j < a.Length; j++)
+                {
+                    try
+                    {
+                        var data = a[j].Split('|');
+                        weight = weight + Convert.ToInt32(data[1]);
+                        way = way + data[0] + ";" + "\n";
+                    }
+                    catch
+                    {
+                        break;
+                    }
+
+
+
+                }
+
+
+
+                ways_weight.Add(new dynamic[2]);
+                ways_weight[i][0] = way;
+                ways_weight[i][1] = weight;
+            }
+
+        }
+
+
+
+        public int FindMax()
+        {
+            int max = ways_weight[0][1], index = 0;
+            for (int i = 0; i < ways_weight.Count; i++)
+            {
+                if (max < ways_weight[i][1])
+                {
+                    max = ways_weight[i][1];
+                    index = i;
+                }
+            }
+            return index;
+        }
+
+
+        public void WriteFile(string path)
+        {
+            StreamWriter sw = new StreamWriter(path, false, Encoding.UTF8);
+            sw.WriteLine(ways_weight[FindMax()][0] + ways_weight[FindMax()][1] + " - длина критического пути");
+            sw.Close();
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Exam/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Hard> ribs = new List<Hard>();
+            StreamReader sr = new StreamReader(@"data.csv");
+            using (sr)
+            {
+                while (sr.EndOfStream != true)
+                {
+                    try
+                    {
+
+                        var a = sr.ReadLine().Split(';');
+                        Hard temp = new Hard();
+                        temp.AddData(Convert.ToInt32(a[0]), Convert.ToInt32(a[1]), Convert.ToInt32(a[2]));
+                        ribs.Add(temp);
+                    }
+                    catch
+                    {
+                        break;
+                    }
+                }
+            }
+
+            int max = ribs[0].in_point;
+            foreach (Hard a in ribs)
+            {
+                if (max < a.in_point)
+                {
+                    max = a.in_point;
+                }
+
+            }
+
+
+            int end_point = max;
+
+            Hard answer = new Hard();
+            foreach (Hard start in ribs.Where(x => x.out_point == 1))
+            {
+
+                answer.FindHardWay(ribs, start, end_point);
+
+            }
+            answer.Parse();
+            answer.WriteFile("Крит.csv");
+        }
+    }
+}
